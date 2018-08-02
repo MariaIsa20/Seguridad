6160b6eb9defcf29e3b2b01317bc8c12146fa759
MaraIsa20 <maria.vanegas.x20@gmail.com>
2018-08-02 15:41:41 -0500

    commit 2

--- hash_md5_sha1.py
+++ hash_md5_sha1.py
@@ -5,9 +5,11 @@ mensaje = str.encode(input('Ingresar mensaje:'))
 # md5 
 m = hashlib.md5()
 m.update(mensaje)
+print("Hash md5:")
 print(m.hexdigest())
 
 # sha1
 s = hashlib.sha1()
 s.update(mensaje)
+print("Hash sha1:")
 print(s.hexdigest())
